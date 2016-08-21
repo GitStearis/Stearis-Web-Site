7897ebaee51389086b4144c96078848312aa66bb
GitStearis <stearis@tut.by>
2016-08-21 23:45:41 +0300

    Footer!
    0) Added footer
    1) Updated info
    2) Reworked up-button
    3) Some small fixes

--- scripts/scrollup.js
+++ scripts/scrollup.js
@@ -1,33 +1,22 @@
 window.onload = function() 
 { 
-    var scrollUp = document.getElementById('button-up-container');
+    var scrollUp = document.getElementById('footer-button');
 
 	scrollUp.onmouseover = function() 
 	{ 
-		scrollUp.style.opacity=0.9;
+		document.body.style.cursor = 
+		scrollUp.style.opacity = 0.9;
 		scrollUp.style.filter  = 'alpha(opacity=80)';
 	};
 
 	scrollUp.onmouseout = function() 
 	{ 
-		scrollUp.style.opacity = 0.5;
+		scrollUp.style.opacity = 0.6;
 		scrollUp.style.filter  = 'alpha(opacity=60)';
 	};
 
 	scrollUp.onclick = function() 
 	{ 
 		window.scrollTo(0,0);
-	};
-	window.onscroll = function () 
-	{ 
-		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-			if ( window.pageYOffset > 250 ) 
-			{
-				scrollUp.style.display = 'block';
-			} 
-			else 
-			{
-				scrollUp.style.display = 'none';
-			}
 	};
 };
